8669b4223afa2de7ce61299ae2281861f8379fe8
Victorlegit <boluvic9@gmail.com>
2026-06-26 15:29:04 +0100

    feat: add clear-cache web route to easily clear config without terminal access

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,3 +7,10 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function() { return 'ok'; });
+
+Route::get('/clear-cache', function() {
+    \Illuminate\Support\Facades\Artisan::call('config:clear');
+    \Illuminate\Support\Facades\Artisan::call('cache:clear');
+    \Illuminate\Support\Facades\Artisan::call('route:clear');
+    return 'Configuration, route, and application cache cleared successfully!';
+});
